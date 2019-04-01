b0c1cb089ae3036f22a2a98cbf0e8e2bc6d05540
Ronen Amiel <ronen.amiel@gmail.com>
2019-04-01 11:38:01 +0300

    add sensible defaults for webpack dev server's allowed hosts

--- packages/yoshi/src/webpack-utils.js
+++ packages/yoshi/src/webpack-utils.js
@@ -166,6 +166,7 @@ function createDevServer(clientCompiler, { publicPath, https, host }) {
     // The server should be accessible externally
     host,
     overlay: true,
+    allowedHosts: ['.wix.com'],
     before(app) {
       // Send cross origin headers
       app.use(cors());
