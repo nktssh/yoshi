940a9c745b61d8e4c2dd2c13dfd1318a5b2aa3da
Nikita Shulipa <mykytas@wix.com>
2019-04-02 18:30:01 +0300

    Fix test access token rights

--- deploy_website_to_surge.sh
+++ deploy_website_to_surge.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-if [type = pull_request && $WEBSITE_CHANGED]
+if [type = pull_request && ${WEBSITE_CHANGED}]
 then
   npx surge --project ./build --domain wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh;
 
   #inspired by https://damien.pobel.fr/post/github-api-from-travisci/
-  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+  CURL_RESULT = curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d "{\"body\": \"You could check new website at http://wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh/yoshi 😉\"}" \
-    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
+  echo ${CURL_RESULT};
 fi;
