e00db2d69dc9ca903c3c04d54020a7ddeaa1eb78
Nikita Shulipa <mykytas@wix.com>
2019-04-03 13:37:32 +0300

    Improve surge website deployment

--- deploy_website_to_surge.sh
+++ deploy_website_to_surge.sh
@@ -5,7 +5,9 @@ then
 
   #inspired by https://damien.pobel.fr/post/github-api-from-travisci/
   CURL_RESULT = curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"You could check new website at http://wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh/yoshi 😉\"}" \
+    -d "{\"body\": \"Deploy preview for Yoshi ready! \
+    \n\n \
+    https://wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh/yoshi \"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
   echo ${CURL_RESULT};
 fi;
