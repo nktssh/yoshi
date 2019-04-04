9d628b0608306ec82a907e861279489a66052cd3
Nikita Shulipa <mykytas@wix.com>
2019-04-04 12:18:02 +0300

    Fix deploy website condition

--- deploy_website_to_surge.sh
+++ deploy_website_to_surge.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-if [type = pull_request && ${WEBSITE_CHANGED}]
-then
-  npx surge --project ./build --domain wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh;
+npx surge --project ./build --domain wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh;
 
-  #inspired by https://damien.pobel.fr/post/github-api-from-travisci/
-  CURL_RESULT = curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"Deploy preview for Yoshi ready! \
-    \n\n \
-    https://wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh/yoshi \"}" \
-    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
-  echo ${CURL_RESULT};
-fi;
+#inspired by https://damien.pobel.fr/post/github-api-from-travisci/
+CURL_RESULT = curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+  -d "{\"body\": \"Deploy preview for Yoshi ready! \
+  \n\n \
+  https://wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh/yoshi \"}" \
+  "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
+echo ${CURL_RESULT};
