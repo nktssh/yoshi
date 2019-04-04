3385184b62de5c8ae76e35620aa457c289d692a8
Nikita Shulipa <mykytas@wix.com>
2019-04-04 12:09:24 +0300

    Update token name used for comments in PRs

--- deploy_website_to_surge.sh
+++ deploy_website_to_surge.sh
@@ -4,7 +4,7 @@ then
   npx surge --project ./build --domain wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh;
 
   #inspired by https://damien.pobel.fr/post/github-api-from-travisci/
-  CURL_RESULT = curl -H "Authorization: token ${GH_TOKEN}" -X POST \
+  CURL_RESULT = curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d "{\"body\": \"Deploy preview for Yoshi ready! \
     \n\n \
     https://wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh/yoshi \"}" \
