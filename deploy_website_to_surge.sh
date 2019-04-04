f95b08f84f872bd459f38239d375bb9e91a7a26e
Nikita Shulipa <mykytas@wix.com>
2019-04-04 12:33:29 +0300

    Debug sending comment via Github API

--- deploy_website_to_surge.sh
+++ deploy_website_to_surge.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 npx surge --project ./website/build --domain wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh;
 
+echo "posting comment about 'https://wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh/yoshi'"
 #inspired by https://damien.pobel.fr/post/github-api-from-travisci/
-CURL_RESULT = curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
   -d "{\"body\": \"Deploy preview for Yoshi ready! \
   \n\n \
   https://wix-yoshi-${TRAVIS_PULL_REQUEST}.surge.sh/yoshi \"}" \
   "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
-echo ${CURL_RESULT};
